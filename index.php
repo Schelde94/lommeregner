aec4e0be91b704e126cfad893b215bc904ea317e
Schelde94 <s.schelde88@gmail.com>
2018-08-23 12:05:19 +0200

    Gange og divisions tegn

--- index.php
+++ index.php
@@ -9,46 +9,52 @@
 	<h1>Bedste lommeregner ever</h1>
 	
 	<form action="<?=$_SERVER['PHP_SELF']?>" method="get">
-		<input type="text" name="val1" >
+		<input type="number" name="val1" required >
 		<br>
-		<input type="text" name="val2" >
+		<input type="number" name="val2" required>
 		<br>
 		<button type="submit" name="operator" value="add">+
 		</button>
 		<button type="submit" name="operator" value="sub">-
 		</button>
-		<button type="submit" name="operator" value="mul">*
+		<button type="submit" name="operator" value="mul">×
 		</button>
-		<button type="submit" name="operator" value="div">/
+		<button type="submit" name="operator" value="div">÷
 		</button>
 	</form>
 	
 	<?php
-		$v1 = $_GET['val1'];
-		$v2 = $_GET['val2'];
+		//$v1 = $_GET['val1'];
+		//$v2 = $_GET['val2'];
+		$v1 = filter_input(INPUT_GET, 'val1', FILTER_VALIDATE_INT) or die ('missing or illegal val1 parameter');
+		$v2 = filter_input(INPUT_GET, 'val2', FILTER_VALIDATE_INT) or die ('missing or illegal val2 parameter');
 		$op = $_GET['operator'];
 		  
-		  
+		
 		  
 		switch($op){
 			case 'add':
 				$res = $v1+$v2;
+				$opchar = "+";
 				break;
 			case 'sub':
 				$res = $v1-$v2;
+				$opchar = "-";
 				break;
 			case 'mul':
 				$res = $v1*$v2;
+				$opchar = "×";
 				break;
 			case 'div':
 				$res = $v1/$v2;
+				$opchar = "÷";
 				break;
 			
 			default: 
 				$res = 'Syntax Error "'.$op.'"';
 		}
 		  
-		  echo $v1.' '.$op.' '.$v2.' = '.$res;
+		  echo $v1.' '.$opchar.' '.$v2.' = '.$res;
 	?>
 </body>
 </html>
