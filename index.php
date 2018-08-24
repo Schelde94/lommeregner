faa47dcb9acaf9b7ce1a1da6d08f60ac4aa44a06
Schelde94 <s.schelde88@gmail.com>
2018-08-24 15:32:31 +0200

    Finished product

--- index.php
+++ index.php
@@ -1,57 +1,63 @@
 <!doctype html>
 <html>
 <head>
-<meta charset="utf-8">
+<meta charset="UTF-8">
 <title>Untitled Document</title>
+<link rel="stylesheet" type="text/css" href="style.css">
 </head>
 
+<?php
+	if (!isset($_GET['operator']))	{
+		$res = 0;
+	}
+	else {
+ 		$op = $_GET['operator'];
+		
+		$v1 = filter_input(INPUT_GET, 'val1', FILTER_VALIDATE_INT);
+		if ($v1 === false) { die('missing or illegal val1 parameter'); }
+		
+		$v2 = filter_input(INPUT_GET, 'val2', FILTER_VALIDATE_INT);
+		if ($v2 === false) { die('missing or illegal val2 parameter'); }
+ 
+		switch($op){
+			case 'add':
+				$res = $v1+$v2;
+				break;
+			case 'sub':
+				$res = $v1-$v2;
+				break;
+			case 'mul':
+				$res = $v1*$v2;
+				break;
+			case 'div':
+				$res = $v1/$v2;
+				break;
+			default:
+				$res = 'Unknown operator "'.$op.'"';
+		}
+	}
+	
+	
+?>
 <body>
 	<h1>Bedste lommeregner ever</h1>
+	<div class="container">
+	
 	
 	<form action="<?=$_SERVER['PHP_SELF']?>" method="get">
-		<input type="number" name="val1" required >
-		<br>
-		<input type="number" name="val2" required>
-		<br>
-		<button type="submit" name="operator" value="add">+
-		</button>
+		<input type="hidden" name="val1" value="<?=$res?>" /><br>
+		<input type="number" value="<?=$res?>" disabled /><br>
+		<input type="number" name="val2" required placeholder="Indtast her" autocomplete="off" /><br>
+		<button type="submit" name="operator" value="add">+</button>
 		<button type="submit" name="operator" value="sub">-</button>
 		<button type="submit" name="operator" value="mul">×</button>
 		<button type="submit" name="operator" value="div">÷</button>
 	</form>
+
+	<form action="<?=$_SERVER['PHP_SELF']?>" method="get">
+		<button type="submit">Reset</button>
+	</form>
+	</div>	
 	
-	<?php
-		//$v1 = $_GET['val1'];
-		//$v2 = $_GET['val2'];
-		$v1 = filter_input(INPUT_GET, 'val1', FILTER_VALIDATE_INT) or die ('missing or illegal val1 parameter');
-		$v2 = filter_input(INPUT_GET, 'val2', FILTER_VALIDATE_INT) or die ('missing or illegal val2 parameter');
-		$op = $_GET['operator'];
-		  
-		
-		  
-		switch($op){
-			case 'add':
-				$res = $v1+$v2;
-				$opchar = "+";
-				break;
-			case 'sub':
-				$res = $v1-$v2;
-				$opchar = "-";
-				break;
-			case 'mul':
-				$res = $v1*$v2;
-				$opchar = "×";
-				break;
-			case 'div':
-				$res = $v1/$v2;
-				$opchar = "÷";
-				break;
-			
-			default: 
-				$res = 'Syntax Error "'.$op.'"';
-		}
-		  
-		  echo $v1.' '.$opchar.' '.$v2.' = '.$res;
-	?>
 </body>
 </html>
